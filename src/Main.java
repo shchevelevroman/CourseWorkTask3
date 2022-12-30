bc8bad1bb586593f9a6702ef451b6f221e8615d2
a123 <shchevelev.roman@yandex.ru>
2022-12-30 18:07:24 +0300

    Задание 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] number;
-        number = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        number = new int[]{1, 2, 3, 4, 5};
         for (int i = number.length - 1; i >= 0; i--) {
             if (i != 0) {
                 System.out.print(number[i] + ", ");
